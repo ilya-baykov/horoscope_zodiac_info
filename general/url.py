c5bba42ca7c88393186117926b98fbce1c4c2c39
ilyab <i-baykov@internet.ru>
2023-02-18 14:05:42 +0300

    Добавил Конвертор Роутов

--- general/url.py
+++ general/url.py
@@ -2,6 +2,7 @@ from django.urls import path
 from general import views
 
 urlpatterns = [
-    path('<zodiac>', views.zodiak_searcher),
+    path('<int:zodiac>', views.zodiak_searcher),
+    path('<str:zodiac>', views.zodiak_searcher),
 
 ]
